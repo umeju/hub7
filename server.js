7550ed9c72d22c4c2a3d2d4b37a7cc176bd90a55
roberto <roberto@roberto-pc.homenet.telecomitalia.it>
2015-07-30 20:37:43 +0200

    interface review + slider

--- server.js
+++ server.js
@@ -49,6 +49,9 @@ app.get('/', function(req, res) {
     res.sendfile(__dirname + '/client/index.html');
 });
 
+app.get('/verardi', function(req, res) {
+    res.sendfile(__dirname + '/verardi/index.html');
+});
 
 
 // Our express application functions as our main listener for HTTP requests
@@ -56,55 +59,6 @@ app.get('/', function(req, res) {
 server = require('http').createServer(app);
 server.listen(PORT, IPADDRESS);
 
-
-
-// Our pool of users.
-var users = {
-    // The actual list of users.
-    list: [],
-    // Add a user to the pool. We do not filter for duplicates.
-    add: function(user) {
-        this.list.push(user);
-        io.sockets.emit('user-list', {
-            'users': this.list
-        });
-    },
-    // Remove a user from the pool.
-    // If the user was actually removed from the pool we return true,
-    // otherwise we return false.
-    remove: function(user) {
-        var index = this.list.indexOf(user);
-        if (index != -1) {
-            // remove our name from the list and update everyone.
-            this.list.splice(index, 1);
-            io.sockets.emit('user-list', {
-                'users': this.list
-            });
-            return true;
-        }
-        // no user removed
-        return false;
-    }
-};
-
-
-
-// List of message templates, compiled as handlebars template functions.
-// To keep with the theme, all messages are handlebars functions, even the
-// ones that don't do substitution.
-var messages = {
-
-    // Do not expect a context object.
-    "welcome": Handlebars.compile('AAA'),
-    "invalidRequireName": Handlebars.compile('BBB'),
-    "invalidNameChange": Handlebars.compile('CCC'),
-    // Expect a context object.
-    "leftChatroom": Handlebars.compile("{{name}} has left the chatroom."),
-    "haveFun": Handlebars.compile('have fun {{name}}.'),
-    "hasJoinedRoom": Handlebars.compile('{{name}} has joined the chatroom.'),
-    "invalidName": Handlebars.compile('sorry, {{name}} DDD')
-
-};
 
 
 
@@ -115,75 +69,24 @@ io.configure(function() {
     var logLevel = (argv["log-level"] === undefined) ? 3 : argv["log-level"];
     io.set("log level", logLevel);
 });
-// Called on a new connection from the client. The socket object should be
-// referenced for future communication with an explicit client.
+
 io.sockets.on('connection', function (socket) {
 
 	console.log("connection!");
     // The username for this socket.
-    var user = User();
-    // Cleans up a bit when we disconnect.
-    var disconnectSocket = function() {
-        var wasUserRemoved = users.remove(user);
-        if (user.name && wasUserRemoved) {
-            io.sockets.emit('chat', Message(messages.leftChatroom(user)));
-        }
-        // We let the client deliver the final response.
-        socket.disconnect();
-    };
+
+	socket.on('left', function(data){
+		console.log('server listen on action ');
+		console.log("data log from server:"+data);
+		io.sockets.emit('left', { action: 'left' });
+	});
+
+	socket.on('right', function(data){
+		console.log('server listen on action ');
+		console.log("data log from server:"+data);
+		io.sockets.emit('right', { action: 'right' });
+	});
 
 
 
-    // Welcome message.
-    socket.emit('chat', Message(messages.welcome()));
-
-
-
-    // Set up listeners on the server side.
-    socket.once('disconnect', function() {
-        // Respond if the client side voluntarily disconnects, but respond
-        // only once. It appears that disconnecting will fire more disconnect
-        // messages, whether from the client or server. So respond once and
-        // only once for each client.
-        disconnectSocket();
-    });
-
-
-    socket.on('set-name', function(data){
-        // Allows a user to set their username.
-        // Very simple username. Must be alphanumeric characters, no spaces.
-        user.name = data.username;
-        if (user.isValid()) {
-            users.add(user);
-            // Update the chatroom.
-            socket.emit('chat', Message(messages.haveFun(user)));
-            io.sockets.emit('chat', Message(messages.hasJoinedRoom(user)));
-        }
-        else {
-            socket.emit('chat', Message(messages.invalidName(user), User('server'), 'error'));
-            disconnectSocket();
-        }
-    });
-
-
-
-    socket.on('chat', function (data) {
-        // Message passed by a client to the server with the intent of
-        // broadcasting to the chatroom.
-        if (data.user && data.user.name == user.name) {
-            io.sockets.emit('chat', Message(data.message, user, "chat"));
-        }
-        else if (!user.name) {
-            // Do not allow people to communicate without having a saved user
-            // name.
-            socket.emit('chat', Message(messages.invalidRequireName(), User('server'), 'error'));
-            disconnectSocket();
-        }
-        else {
-            // Do not allow people to change their name in the request alone
-            // on this socket. (Example of a very pathetic amount of security).
-            socket.emit('chat', Message(messages.invalidNameChange(), User('server'), 'error'));
-            disconnectSocket();
-        }
-    });
 });
