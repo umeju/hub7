64f034ba68e68b66b04998e67e04981a3832faa3
hubanero <roborlan@gmail.com>
2016-11-17 17:30:16 +0100

    pubblicazione calasso con css in sito di-vision2

---
+++ pages/vuota.php
@@ -0,0 +1,188 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>home bardinella</title>
+
+        <meta name="description" content="Source code generated using layoutit.com">
+        <meta name="author" content="roberto" >
+
+        <link href="../css/bootstrap.min.css" rel="stylesheet">
+        <link href="../css/style.css" rel="stylesheet">
+        <link rel="stylesheet" href="../venobox/venobox.css" type="text/css" media="screen" />
+
+    </head>
+    <body>
+
+        <div class="container-fluid">
+			
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="jumbotron">
+                        <div class="media-body">
+                            <a href="/~di-vision/home-bardinella">
+                                <img id="cazzo" src="img/bg.jpg" />
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>	
+            
+            
+			<div class="row">
+                
+                <div class="col-md-12">
+                    <div class="media well">
+                        <div class="media-body">
+                            <div class="media">
+                                <div class="media-body">
+                                    <p class="text-center">
+                                        <a class="venobox" data-gall="myGallery" href="../img/unox.jpg">
+                                            <img class="hub-thumb" src="../img/artic.jpg" />
+                                        </a>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+            
+            
+
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="media well">
+                        <div class="media-body">
+                            <div class="media">
+                                <div class="media-body">
+                                    <p class="text-center">
+                                        <a class="venobox" data-gall="myGallery" href="img/1.jpg">
+                                            <img class="hub-thumb" src="img/thumb1.jpg" />
+                                        </a>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="media well">
+                        <div class="media-body">
+                            <div class="media">
+                                <div class="media-body">
+                                    <p class="text-center">
+                                            <a class="venobox" data-gall="myGallery" href="img/2.jpg">
+                                                <img class="hub-thumb" src="img/thumb2.jpg" />
+                                            </a>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4">
+                    <div class="media well">
+                        <div class="media-body">
+                            <div class="media">
+                                <div class="media-body">
+                                    <p class="text-center">
+                                        <a class="venobox" data-gall="myGallery" href="img/3.jpg">
+                                            <img class="hub-thumb" src="img/thumb3.jpg" />
+                                        </a>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+
+            </div>
+
+
+
+
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="media well">
+                        <div class="media-body">
+                            <div class="media">
+                                <div class="media-body">
+                                    <p class="text-center">
+                                        <a class="venobox" data-gall="myGallery" href="img/4.jpg">
+                                            <img class="hub-thumb" src="img/thumb4.jpg" />
+                                        </a>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="media well">
+                        <div class="media-body">
+                            <div class="media">
+                                <div class="media-body">
+                                    <p class="text-center">
+                                        <a class="venobox" data-gall="myGallery" href="img/5.jpg">
+                                            <img class="hub-thumb" src="img/thumb5.jpg" />
+                                        </a>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="media well">
+                        <div class="media-body">
+                            <div class="media">
+                                <div class="media-body">
+                                    <p class="text-center">
+                                        <a class="venobox" data-gall="myGallery" href="img/6.jpg">
+                                            <img class="hub-thumb" src="img/thumb6.jpg" />
+                                        </a>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+
+
+
+
+            <div class="row">
+                <div class="col-md-12">
+                    <a href="#">
+                        <img id="cazzo" src="../img/splash.png" />
+                    </a>
+
+                </div>
+            </div>
+
+                <p class="text-center">
+                <a href="#">
+                    <img id="splash" src="../img/splash.png" />
+                </a>
+            </p>
+
+
+        </div>
+
+        <script src="../js/jquery.min.js"></script>
+        <script src="../js/bootstrap.min.js"></script>
+        <script src="../js/scripts.js"></script>
+        <script type="text/javascript" src="../js/venobox.min.js"></script>
+    </body>
+</html>
