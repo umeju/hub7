4d32806b730362a8f65e33cc065ab6e578e9b7d3
hubanero <roborlan@gmail.com>
2017-02-16 00:37:33 +0100

    vanini bottom frame with margin and padding + distance in % from -5 to -1 + font-size 2.7em

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ var PORT = process.env.OPENSHIFT_INTERNAL_PORT
 var IPADDRESS = process.env.OPENSHIFT_INTERNAL_IP
 	//	|| process.env.OPENSHIFT_NODEJS_IP || '192.168.1.104' || '127.0.0.1';
 	//	|| process.env.OPENSHIFT_NODEJS_IP || '192.168.43.74' || '127.0.0.1';
-        //      || process.env.OPENSHIFT_NODEJS_IP || '192.168.1.111' || '127.0.0.1';
-	      || process.env.OPENSHIFT_NODEJS_IP || '192.168.1.126' || '127.0.0.1';
+              || process.env.OPENSHIFT_NODEJS_IP || '192.168.1.104' || '127.0.0.1';
+//	      || process.env.OPENSHIFT_NODEJS_IP || '192.168.1.126' || '127.0.0.1';
 
 var express = require('express');
 //var reload = require('reload');
