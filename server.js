030d4c8325d47dca88217909504c8085662f4be4
hubanero <roborlan@gmail.com>
2017-06-10 11:34:59 +0200

    promemoria testo bottom

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ var IPADDRESS = process.env.OPENSHIFT_INTERNAL_IP
 	//	|| process.env.OPENSHIFT_NODEJS_IP || '192.168.1.104' || '127.0.0.1';
 	//	|| process.env.OPENSHIFT_NODEJS_IP || '192.168.43.74' || '127.0.0.1';
 
-//                || process.env.OPENSHIFT_NODEJS_IP || '192.168.1.40' || '127.0.0.1';
-	      || process.env.OPENSHIFT_NODEJS_IP || '192.168.1.103' || '127.0.0.1';
+                || process.env.OPENSHIFT_NODEJS_IP || '192.168.1.215' || '127.0.0.1';
+//	      || process.env.OPENSHIFT_NODEJS_IP || '192.168.1.103' || '127.0.0.1';
 
 
 var express = require('express');
